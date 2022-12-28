170a36a87081688cf50efa4064b1fc304fcae5ff
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-12-28 17:48:40 +0700

    add function case 1-5

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -132,7 +132,35 @@ namespace BinarySearchTree
                 Console.WriteLine();
                 switch (ch)
                 {
-
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
                 }
             }
         }
