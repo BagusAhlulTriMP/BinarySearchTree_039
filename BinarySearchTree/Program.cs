99941fa3c9fbb9e2ad83735ebf56f564e57c56e7
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-12-28 17:18:21 +0700

    add function compare

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -33,6 +33,16 @@ namespace BinarySearchTree
         public void search(string element, ref Node parent, ref Node currentNode)
         {
             /* This function searchs the currentNode of the specified  Node as well as the current Node of its parents */
+            currentNode = ROOT;
+            parent = null;
+            while((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
         }
     }
 }
